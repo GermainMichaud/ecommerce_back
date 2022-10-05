5d47dc4e51c2620e961a8163d07b354bcc0285fb
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-05 15:44:47 +0200

    feat: add session middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,11 +1,36 @@
 import compression from 'compression';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application } from 'express';
+import session from 'express-session';
 import helmet from 'helmet';
+import { nanoid } from 'nanoid';
 
 const loadMiddlewares = (app: Application) => {
   app.use(helmet());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    }),
+  );
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        secure: false,
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7 * 1000, // 1 week
+        path: '/',
+      },
+      genid: () => {
+        return nanoid(64);
+      },
+    }),
+  );
+  app.use(cookieParser('secret'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(compression());
