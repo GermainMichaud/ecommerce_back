803c095c4fa33984ac144f3b57c1a47832df0c73
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-05 15:48:27 +0200

    feat: add DB connection & seed

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
-import config from 'config';
 import * as dotenv from 'dotenv';
+dotenv.config();
+import config from 'config';
 
 import createServer from './server';
+import connect from './utils/db';
 import logger from './utils/logger';
+import { seed } from './utils/seeder';
 import swaggerDocs from './utils/swagger';
 
-dotenv.config();
-
-const startServer = () => {
+const startServer = async () => {
   const server = createServer();
 
   const PORT = config.get<number>('port');
 
   try {
-    server.listen(PORT, () => {
-      logger.info(`Server is running on port ${PORT}`);
-      swaggerDocs(server, PORT);
-    });
+    server.listen(PORT);
+    logger.info(`Server is running on port ${PORT}`);
+    swaggerDocs(server, PORT);
+    await connect();
+    if (process.argv.includes('seed')) {
+      logger.info('Start seeding');
+      await seed();
+      logger.info('Seeding completed');
+    }
   } catch (error) {
     logger.error(error);
     process.exit(1);
   }
 };
 
-startServer();
+void (async () => {
+  await startServer();
+})();
