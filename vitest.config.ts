72860c1cc4ac5a91829e7b01bafe2ea091c4050c
GermainMichaud <gmichaud.dev@gmail.com>
2022-10-05 15:46:24 +0200

    feat: update vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,12 +7,15 @@ export default defineConfig({
     setupFiles: ['./src/utils/setupTests.ts'],
     watch: false,
     reporters: ['verbose'],
+    include: ['src/**/*.test.ts'],
+    exclude: ['src/utils/seeder.ts', 'node_modules'],
     coverage: {
       include: ['src/**/*.ts'],
       exclude: [
         'src/**/*.{test,spec}.ts',
         'src/utils/setupTests.ts',
         'src/utils/swagger.ts',
+        'src/utils/seeder.ts',
       ],
       provider: 'c8',
       all: true,
