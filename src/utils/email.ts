c91fa424ec83931a370b6bc62916203f6a986023
Germain Michaud <gmichaud.dev@gmail.com>
2022-10-07 16:40:06 +0200

    feat: remove try/catch & fix types

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,45 +1,41 @@
 import config from 'config';
-import { Seller } from 'models/seller.model';
 import nodemailer from 'nodemailer';
 
 import { Order } from '../models/order.model';
+import { Seller } from '../models/seller.model';
 import log from './logger';
 
 export const sendOrderConfirmationEmailToCustomer = async (order: Order) => {
-  try {
-    const account = await nodemailer.createTestAccount();
-    const createTransporter = nodemailer.createTransport(
-      {
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-          user: account.user,
-          pass: account.pass,
-        },
-        logger: false,
-        debug: false,
+  const account = await nodemailer.createTestAccount();
+  const createTransporter = nodemailer.createTransport(
+    {
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
       },
-      {
-        from: `Ecommerce Application ${config.get<string>('email.from')}`,
-      },
-    );
-    const mailOptions = {
-      to: order.userInfos.email,
-      subject: 'Order Confirmation',
-      html: `
+      logger: false,
+      debug: false,
+    },
+    {
+      from: `Ecommerce Application ${config.get<string>('email.from')}`,
+    },
+  );
+  const mailOptions = {
+    to: order.userInfos.email,
+    subject: 'Order Confirmation',
+    html: `
         <h1>Order Confirmation</h1>
         <p>Thank you for your order, ${order.userInfos.lastname} ${order.userInfos.firstname}.</p>
         <p>Your order number is: ${order.orderToken}</p>
         <p>Thank you for shopping with us.</p>
       `,
-    };
-    const info = await createTransporter.sendMail(mailOptions);
-    log.info('Message sent: %s', info.messageId);
-    log.info('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  } catch (error) {
-    throw new Error(error);
-  }
+  };
+  const info = await createTransporter.sendMail(mailOptions);
+  log.info('Message sent: %s', info.messageId);
+  log.info('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 };
 
 export const sendPurchaseOrderEmailToSeller = async (
@@ -53,42 +49,35 @@ export const sendPurchaseOrderEmailToSeller = async (
     postal_code: string;
     country: string;
   },
-  items: {
-    product: string;
-    _id: string;
-    quantity: number;
-    price: number;
-    name: string;
-  }[],
+  items: Order['items'],
   orderStatus: string,
 ) => {
-  try {
-    const account = await nodemailer.createTestAccount();
-    const createTransporter = nodemailer.createTransport(
-      {
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-          user: account.user,
-          pass: account.pass,
-        },
-        logger: false,
-        debug: false,
+  const account = await nodemailer.createTestAccount();
+  const createTransporter = nodemailer.createTransport(
+    {
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
       },
-      {
-        from: `Ecommerce Application ${config.get<string>('email.from')}`,
-      },
-    );
-    const mailOptions = {
-      to: sellerInfos.email,
-      subject: 'Purchase Order',
-      html: `
+      logger: false,
+      debug: false,
+    },
+    {
+      from: `Ecommerce Application ${config.get<string>('email.from')}`,
+    },
+  );
+  const mailOptions = {
+    to: sellerInfos.email,
+    subject: 'Purchase Order',
+    html: `
         <h1>Purchase Order</h1>
         <p>Dear ${sellerInfos.name},</p>
         <p>You have received a new order from ${userInfos.lastname} ${
-        userInfos.firstname
-      }.</p>
+      userInfos.firstname
+    }.</p>
         <p>Order details:</p>
         <ul>
           ${items
@@ -96,7 +85,7 @@ export const sendPurchaseOrderEmailToSeller = async (
               (item) => `
             <li>
               <p>${item.name}</p>
-              <p>SKU: ${item.product} - ${item._id}</p>
+              <p>SKU: ${item.product.toString()} - ${item._id.toString()}</p>
               <p>Quantity: ${item.quantity}</p>
               <p>Price: ${item.price}</p>
             </li>
@@ -107,11 +96,8 @@ export const sendPurchaseOrderEmailToSeller = async (
         <p>Order status: ${orderStatus}</p>
         <p>Thank you for your business.</p>
       `,
-    };
-    const info = await createTransporter.sendMail(mailOptions);
-    log.info('Message sent to seller %s: %s', sellerInfos._id, info.messageId);
-    log.info('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  } catch (error) {
-    throw new Error(error);
-  }
+  };
+  const info = await createTransporter.sendMail(mailOptions);
+  log.info('Message sent to seller %s: %s', sellerInfos._id, info.messageId);
+  log.info('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 };
