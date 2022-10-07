d0150790b389eef5f4f6efcc554fef2a20116ddc
Germain Michaud <gmichaud.dev@gmail.com>
2022-10-07 16:41:56 +0200

    feat: add error middleware to server & type cart in session

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 
 import loadMiddlewares from './middlewares';
+import { errorMiddleware } from './middlewares/error';
 import loadRoutes from './routes';
+
+declare module 'express-session' {
+  interface SessionData {
+    cart?: Record<string, unknown>;
+  }
+}
 
 const createServer = () => {
   const app = express();
@@ -9,6 +16,8 @@ const createServer = () => {
   loadMiddlewares(app);
 
   loadRoutes(app);
+
+  app.use(errorMiddleware);
 
   return app;
 };
