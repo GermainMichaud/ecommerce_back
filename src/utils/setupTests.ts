4797317c4527fbc7bd9c774ab56e9d7288c127a2
Germain Michaud <gmichaud.dev@gmail.com>
2022-10-09 23:34:00 +0200

    fix: set correct env file for setupTests & remove mongo memory server

--- src/utils/setupTests.ts
+++ src/utils/setupTests.ts
@@ -1,19 +1,4 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import { afterAll, beforeAll } from 'vitest';
-
-import { seed } from './seeder';
-
-let mongoServer: MongoMemoryServer;
-
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  await mongoose.connect(mongoServer.getUri());
-  await seed();
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoose.connection.close();
-  await mongoServer.stop();
+import * as dotenv from 'dotenv';
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
